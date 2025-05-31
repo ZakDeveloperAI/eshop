39074739cdf409c6ba6bd251379734017c9aa126
ZakDeveloperAI <belfakhirzakaria@gmail.com>
2025-05-31 11:36:29 +0200

    set up proxy, local api and ports collision

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -4,15 +4,46 @@
  */
 
 import express from 'express';
-import * as path from 'path';
-
+import cors from"cors"
+import proxy from 'express-http-proxy';
+import morgan from 'morgan';
+import rateLimit from 'express-rate-limit';
+import swaggerUi from 'swagger-ui-express';
+import axios from 'axios';
+import cookieParser from 'cookie-parser';
 const app = express();
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')))
+app.use(cors({
+  origin: ['http://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
 
-app.get('/api', (req, res) => {
+app.use(morgan('dev'));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
+app.use(cookieParser());
+app.set('trust proxy', 1); // trust first proxy
+
+//Apply rate limiting
+const limiter=rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: (req:any) => (req.user?1000:100),  // limit each IP to 100 requests per windowMs
+  message: 'Too many requests from this IP, please try again later',
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: true, // Disable the `X-RateLimit-*` headers
+  keyGenerator: (req:any) => req.ip, // Use IP address as the key
+});
+
+app.use(limiter);
+
+app.get('/gateway-health', (req, res) => {
   res.send({ message: 'Welcome to api-gateway!' });
 });
+
+
+app.use('/',proxy('http://localhost:6001'));
 
 const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
